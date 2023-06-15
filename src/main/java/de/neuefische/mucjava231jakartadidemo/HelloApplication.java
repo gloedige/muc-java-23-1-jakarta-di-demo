ce252fc081df3f4ae7d33c9d04bb512987acd089
Marcell <marcell.dechant@neuefische.de>
2023-06-15 11:16:10 +0200

    Delete Hello

--- src/main/java/de/neuefische/mucjava231jakartadidemo/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package de.neuefische.mucjava231jakartadidemo;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
